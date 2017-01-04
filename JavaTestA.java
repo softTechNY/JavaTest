1e9150548cc23934fcf627f0e79af8fe0b1bddf1
mdhossain62 <mhossain456@gmail.com>
2017-01-04 18:16:27 -0500

    Test Commit

---
+++ JavaTestA.java
@@ -0,0 +1,11 @@
+public class JavaTestA{
+public static void main(String []args) {
+int num1=12;
+int num2=8;
+int sum= num1+ num2;
+
+System.out.println("Sum is " + sum);
+System.out.println("This is good");
+
+}
+}
